391273bf1c0dbef11688e1967592d57cef803a54
RobiChiri <robertochiriac8@gmail.com>
2023-11-10 16:28:00 +0100

    a

--- esercizio.js
+++ esercizio.js
@@ -17,7 +17,7 @@
 // const bankAccount = {
 // 	balance: 500,
 //   holdersName: "John Doe",
-  
+
 // 	depositMoney(money) {
 // 		this.balance += money;
 // 	},
@@ -32,7 +32,6 @@
 
 // bankAccount.withdrawMoney(200);
 // console.log(bankAccount.balance);
-
 
 // Create two objects with similar properties. Write a function that compares the two objects and checks if they have the same properties and values.
 
@@ -60,3 +59,88 @@
 // }
 
 // console.log(checkEqual(object1, object2));
+
+// Given an object person with the properties name and age, use object destructuring to create variables name and age that hold the values from the object.
+
+// let Person = {
+//   name: "Robi",
+//   age: 20
+// }
+
+// let {name, age} = Person;
+
+// console.log(name);
+// console.log(age);
+
+// Given a nested object student with properties info and grades, use object destructuring to create variables name, age, and grade that hold the values from the nested object.
+// const student = {
+// 	info: { name: "Bob", age: 25 },
+// 	grades: { math: 95, science: 89 },
+// };
+
+// let {
+// 	info: { name, age },
+// 	grades: { math, science },
+// } = student;
+
+// console.log(name);
+// console.log(age);
+// console.log(grades.math);
+// console.log(grades.science);
+
+// Given an array colors with three elements, use array destructuring to create variables firstColor, secondColor, and thirdColor that hold the values from the array.
+// const colors = ["red", "green", "blue"];
+
+// let [firstColor, secondColor, thirdColor] = colors;
+
+// console.log(firstColor);
+// console.log(secondColor);
+// console.log(thirdColor);
+
+// Create a function called sum that accepts any number of arguments and returns the sum of those arguments. Use the rest parameter to collect the arguments.
+
+// function sum(...parameters) {
+//   let result = 0;
+//   for (parameter of parameters) {
+//     sum += parameter;
+//   }
+//   return sum;
+// }
+
+// console.log(sum(5, 3, 8));
+
+// Write a function filterOutOdds that accepts any number of arguments and returns an array containing only the even numbers. Use the rest parameter to collect the arguments and the spread operator to construct the new array.
+
+// function filterOutOdds(...args) {
+// 	return args.filter(number => number % 2 == 0);
+// }
+
+// console.log(filterOutOdds(2, 3, 5, 6, 1));
+
+// Given two arrays, arr1 and arr2, create a new array mergedArray using the spread operator that combines the elements of both arrays.
+
+// function mergedArray(arr1, arr2) {
+// 	let mergedArray = [...arr1, ...arr2];
+// 	return mergedArray;
+// }
+
+// let arr1 = [3, 5, 7];
+// let arr2 = [2, 4, 6];
+// console.log(mergedArray(arr1, arr2));
+
+
+// Given two objects, obj1 and obj2, create a new object mergedObject using the spread operator that combines the properties of both objects. If there are overlapping properties, the values from obj2 should overwrite the values from obj1.
+
+// let obj1 = {
+// 	prop1: "value1",
+// 	prop2: "value2",
+// }
+
+// let obj2 = {
+// 	prop2: "value3",
+// 	prop4: "value4",
+// };
+
+// let mergedObject = { ...obj1, ...obj2 };
+
+// console.log(mergedObject);
